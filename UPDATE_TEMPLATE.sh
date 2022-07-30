19d3d1d89e65ca6cc226f8dd7ebc12efb8fd8b52
Samuel Voeller <samuel.voeller@amethyst.live>
2022-07-30 17:26:24 -0400

    feat: add .github

--- UPDATE_TEMPLATE.sh
+++ UPDATE_TEMPLATE.sh
@@ -6,7 +6,7 @@ echo "Cloning the 'meta' template to the relative storage."
 git clone https://github.com/amethyst-studio/meta "${TEMP_DIR}"
 
 echo "Pulling the files which can be updated."
-cp -r "./${TEMP_DIR}/ISSUE_TEMPLATE" "./"
+cp -r "./${TEMP_DIR}/.github" "./"
 cp -r "./${TEMP_DIR}/CODE_OF_CONDUCT.md" "./CODE_OF_CONDUCT.md"
 cp -r "./${TEMP_DIR}/CONTRIBUTING.md" "./CONTRIBUTING.md"
 cp -r "./${TEMP_DIR}/SECURITY.md" "./SECURITY.md"
